b5f0a832460c0f8d06995dc70c21b141a2d0043c
Drew Ross <mail.drew.ross@gmail.com>
2020-09-14 14:21:54 -0700

    Complete searching.py. Test OK.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,22 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    if len(arr) > 0:
+        for i, num in enumerate(arr):
+            if num == target:
+                return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    if len(arr) > 0:
+        left = 0
+        right = len(arr) - 1
+        while right >= left:
+            middle = (left + right) // 2
+            if target == arr[middle]:
+                return middle
+            elif target < arr[middle]:
+                right = middle - 1
+            else:
+                left = middle + 1
     return -1  # not found
