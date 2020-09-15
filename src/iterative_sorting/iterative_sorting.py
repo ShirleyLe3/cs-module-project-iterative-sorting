39aa2b3f780d05104648d7447102d9905f97a8dd
Drew Ross <mail.drew.ross@gmail.com>
2020-09-15 09:53:05 -0700

    Refactor for readability.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,21 +1,23 @@
 def selection_sort(arr):
-    if arr:
-        for i in range(len(arr)):
-            smallest_index = i
-            for j in range(i + 1, len(arr)):
-                if arr[j] < arr[smallest_index]:
-                    smallest_index = j
-            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
+    if not arr:
+        return arr
+    for i in range(len(arr)):
+        smallest_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    if arr:
-        for i in range(len(arr)):
-            for j in range(len(arr) - i - 1):
-                if arr[j] > arr[j + 1]:
-                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    if not arr:
+        return arr
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
 
@@ -39,23 +41,24 @@ What is the time and space complexity of the counting sort algorithm?
 
 
 def counting_sort(arr, maximum=None):
-    if arr:
-        if not maximum:
-            maximum = max(arr)
+    if not arr:
+        return arr
+    
+    if not maximum:
+        maximum = max(arr)
 
-        count = [0] * (maximum + 1)
-        sum_count = count.copy()
-        output = [0] * len(arr)
-        
-        for num in arr:
-            if num < 0:
-                return "Error, negative numbers not allowed in Count Sort"
-            count[num] += 1
-        for i in range(len(count)):
-            sum_count[i] = sum_count[i - 1] + count[i]
-        for num in arr:
-            output[sum_count[num] - 1] = num
-            sum_count[num] -= 1
-        arr = output
+    count = [0] * (maximum + 1)
+    sum_count = count.copy()
+    output = [0] * len(arr)
+    
+    for num in arr:
+        if num < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        count[num] += 1
+    for i in range(len(count)):
+        sum_count[i] = sum_count[i - 1] + count[i]
+    for num in arr:
+        output[sum_count[num] - 1] = num
+        sum_count[num] -= 1
 
-    return arr
+    return output
